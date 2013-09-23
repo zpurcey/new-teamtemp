52d81e3676a6fc185b72e5f1f8582d12bc355730
Will Thames <will@thames.id.au>
2013-09-23 19:58:41 +1000

    Added about page

--- teamtemp/urls.py
+++ teamtemp/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from teamtemp.views import home, admin, submit
 
 urlpatterns = patterns('',
     url(r'^$', home),
+    url(r'^about$', TemplateView.as_view(template_name='about.html'), 
+        name='about'),
     url(r'^admin/([0-9a-zA-Z]{8})$', admin),
     url(r'^([0-9a-zA-Z]{8})$', submit),
 )
