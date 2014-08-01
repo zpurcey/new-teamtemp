6e916526dc2488a7895cfbd34cbf088bbd8b0b98
zpurcey <github@purcellshouse.com>
2014-08-01 23:39:00 +1000

    cron script missing?

--- bin/archive_scores.py
+++ bin/archive_scores.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import requests
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teamtemp.settings")
 
-print 'hello world'
+    url = os.environ.get('TEAM_TEMP_CRON_URL', 'http://127.0.0.1:5000/cron/')
+    pin = os.environ.get('TEAM_TEMP_CRON_PIN', '0000')
+
+    r = requests.get(url + pin)
+    print r.status_code
+
