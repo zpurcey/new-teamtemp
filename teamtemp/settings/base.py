b2b3e861586e4d193de18b55a2d88184cc5c80f0
Robert Loomans <robert@loomans.org>
2016-06-17 15:46:46 +1000

    Set API page size

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -201,6 +201,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'PAGE_SIZE': 10
 }
-
