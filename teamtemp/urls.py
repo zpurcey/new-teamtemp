3418d2039bdeba71ac5b1211c3ca44f4f945d646
Robert Loomans <robert@loomans.org>
2016-12-08 17:52:07 +1000

    dont use deprecated functionality

--- teamtemp/urls.py
+++ teamtemp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.views.static import serve
@@ -14,33 +14,33 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^about$', TemplateView.as_view(template_name='about.html'),
         name='about'),
-    url(r'^admin/(?P<survey_id>[0-9a-zA-Z]{8})$', admin),
-    url(r'^set/(?P<survey_id>[0-9a-zA-Z]{8})$', set),
-    url(r'^admin/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', admin),
-    url(r'^(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', submit),
+    url(r'^admin/(?P<survey_id>[0-9a-zA-Z]{8})$', admin, name='admin'),
+    url(r'^set/(?P<survey_id>[0-9a-zA-Z]{8})$', set, name='set'),
+    url(r'^admin/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', admin, name='admin'),
+    url(r'^(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', submit, name='submit'),
 
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/i(?P<num_iterations>[0-9]{1,3})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})/i(?P<num_iterations>[0-9]{1,3})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})/(?P<archive_id>[0-9]{1,20})$', bvc),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/i(?P<num_iterations>[0-9]{1,3})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})/i(?P<num_iterations>[0-9]{1,3})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})/(?P<archive_id>[0-9]{1,20})$', bvc, name='bvc'),
 
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/region=(?P<region_names>[-\w\,]{1,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/site=(?P<site_names>[-\w\,]{1,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/dept=(?P<dept_names>[-\w\,]{1,64})$', bvc),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/region=(?P<region_names>[-\w\,]{1,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/site=(?P<site_names>[-\w\,]{1,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/dept=(?P<dept_names>[-\w\,]{1,64})$', bvc, name='bvc'),
 
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/dept=(?P<dept_names>[-\w\,]{0,64})/region=(?P<region_names>[-\w\,]{0,64})/site=(?P<site_names>[-\w\,]{0,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/dept=(?P<dept_names>[-\w\,]{0,64})/region=(?P<region_names>[-\w\,]{0,64})/site=(?P<site_names>[-\w\,]{0,64})$', bvc),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/dept=(?P<dept_names>[-\w\,]{0,64})/region=(?P<region_names>[-\w\,]{0,64})/site=(?P<site_names>[-\w\,]{0,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/dept=(?P<dept_names>[-\w\,]{0,64})/region=(?P<region_names>[-\w\,]{0,64})/site=(?P<site_names>[-\w\,]{0,64})$', bvc, name='bvc'),
 
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/region=(?P<region_name>[-\w\,]{1,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/site=(?P<site_name>[-\w\,]{1,64})$', bvc),
-    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/dept=(?P<dept_name>[-\w\,]{1,64})$', bvc),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/region=(?P<region_name>[-\w\,]{1,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/site=(?P<site_name>[-\w\,]{1,64})$', bvc, name='bvc'),
+    url(r'^bvc/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<archive_id>[0-9]{1,20})/dept=(?P<dept_name>[-\w\,]{1,64})$', bvc, name='bvc'),
 
-    url(r'^reset/(?P<survey_id>[0-9a-zA-Z]{8})$', reset),
+    url(r'^reset/(?P<survey_id>[0-9a-zA-Z]{8})$', reset, name='reset'),
     url(r'^cron/(?P<pin>[0-9]{4})$', cron),
-    url(r'^team/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', team),
-    url(r'^team/(?P<survey_id>[0-9a-zA-Z]{8})$', team),
+    url(r'^team/(?P<survey_id>[0-9a-zA-Z]{8})/(?P<team_name>[-\w]{1,64})$', team, name='team'),
+    url(r'^team/(?P<survey_id>[0-9a-zA-Z]{8})$', team, name='team'),
     url(r'^filter/(?P<survey_id>[0-9a-zA-Z]{8})$', filter),
     url(r'^static/(.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     url(r'^media/(.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
