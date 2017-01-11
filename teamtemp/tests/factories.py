f1de9ef914026c28ff3c98ba2b906a870e5db70b
Robert Loomans <robert.loomans@suncorp.com.au>
2017-01-11 10:34:06 +1000

    most factories done

--- teamtemp/tests/factories.py
+++ teamtemp/tests/factories.py
@@ -4,11 +4,11 @@ import factory
 import factory.fuzzy
 
 from django.contrib.auth.hashers import make_password
+from django.utils import timezone
 
 from teamtemp import utils
 from teamtemp.responses.models import TeamResponseHistory, Teams, TeamTemperature, TemperatureResponse, User, WordCloudImage
 
-# request.session[USER_ID_KEY] = utils.random_string(32)
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -34,11 +34,7 @@ class TeamFactory(factory.django.DjangoModelFactory):
         django_get_or_create = ('team_name',)
 
     request = factory.SubFactory(TeamTemperatureFactory)
-    responder = factory.SubFactory(UserFactory)
-    score = factory.Sequence(lambda n: n%10 + 1)
-    word = factory.fuzzy.FuzzyText(length=32, chars=string.ascii_letters)
-    response_date = 
-    team_name = factory.SubFactory(TeamsFactory).team_name
+    name = factory.Sequence(lambda n: 'Tag-%d' % n)
 
 
 class TemperatureResponse(factory.django.DjangoModelFactory):
@@ -46,68 +42,21 @@ class TemperatureResponse(factory.django.DjangoModelFactory):
         model = TemperatureResponse
 
     request = factory.SubFactory(TeamTemperatureFactory)
-    name = factory.Sequence(lambda n: 'Tag-%d' % n)
+    responder = factory.SubFactory(UserFactory)
+    score = factory.FuzzyInteger(1, 10)
+    word = factory.fuzzy.FuzzyText(length=32, chars=string.ascii_letters)
+    response_date = factory.LazyFunction(timezone.now())
+    team = factory.SubFactory(TeamsFactory)
+    team_name = factory.LazyAttribute(lambda a: a.team.team_name)
 
 
-class TeamFactory(factory.django.DjangoModelFactory):
+class TeamResponseHistoryFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Team
+        model = TeamResponseHistory
 
+    request = factory.SubFactory(TeamTemperatureFactory)
+    average_score = factory.FuzzyFloat(1, 10)
+    responder_count = factory.FuzzyInt(1,25)
+    word_list = factory.LazyAttribute(lambda a: (factory.fuzzy.FuzzyText(length=factory.FuzzyInt(2,32), chars=string.ascii_letters) for i in range(1, a.responder_count))
     name = factory.Sequence(lambda n: 'Team %d' % n)
     short_desc = 'Test Team Short Description'
-
-    @factory.post_generation
-    def tags(self, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if extracted:
-            # A list of groups were passed in, use them
-            for tag in extracted:
-                self.tags.add(tag)
-
-
-class AssessmentFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Assessment
-
-    template = factory.SubFactory(TemplateFactory)
-    team = factory.SubFactory(TeamFactory)
-
-    @factory.post_generation
-    def tags(self, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if extracted:
-            # A list of groups were passed in, use them
-            for tag in extracted:
-                self.tags.add(tag)
-
-
-class RatingFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Rating
-
-    attribute = factory.SubFactory(AttributeFactory)
-    name = factory.Sequence(lambda n: 'Rating %d' % n)
-    desc = "This is a really good description."
-    rank = factory.Sequence(lambda n: n)
-
-
-class MeasurementFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Measurement
-
-    assessment = factory.SubFactory(AssessmentFactory)
-    rating = factory.SubFactory(RatingFactory)
-    observations = factory.fuzzy.FuzzyText(length=256, chars=string.ascii_letters)
-
-
-class AnnouncementFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Announcement
-
-    title = factory.Sequence(lambda n: 'Announcement-%d' % n)
-    content = "This is the really good content for %s\n" % title
-
