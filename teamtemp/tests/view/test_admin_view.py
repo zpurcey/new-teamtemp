220f50fd1af6800f8d5e90a0a81d2f55db97a2b5
Robert Loomans <robert@loomans.org>
2017-01-23 21:18:39 +1000

    add authorized admin view tests

--- teamtemp/tests/view/test_admin_view.py
+++ teamtemp/tests/view/test_admin_view.py
@@ -1,10 +1,8 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from teamtemp import responses
 from teamtemp.tests.factories import TeamTemperatureFactory, TeamFactory
-
-
-# No admin auth yet, so all admin functions should render the password form
 
 
 class AdminViewTestCases(TestCase):
@@ -13,29 +11,68 @@ class AdminViewTestCases(TestCase):
         self.team = TeamFactory(request=self.teamtemp)
         self.admin_url = reverse('admin', kwargs={'survey_id': self.teamtemp.id})
 
-    def _assert_password_form(self, response):
+    def assertIsPasswordForm(self, response):
         self.assertContains(response, 'Enter the admin password')
         self.assertTemplateUsed(response, 'password.html')
 
-    def _assert_redirects(self, response):
+    def assertDoesAdminRedirect(self, response):
         self.assertRedirects(response, self.admin_url)
-        self._assert_password_form(response)
+        self.assertIsPasswordForm(response)
+
+    def setUpAdmin(self):
+        session = self.client.session
+        session[responses.ADMIN_KEY] = [self.teamtemp.id]
+        session.save()
+
+    def test_no_admin_no_team_view(self):
+        response = self.client.get(self.admin_url)
+        self.assertIsPasswordForm(response)
 
     def test_admin_no_team_view(self):
+        self.setUpAdmin()
         response = self.client.get(self.admin_url)
-        self._assert_password_form(response)
+        self.assertContains(response, "Admin %s" % self.teamtemp.id)
+        self.assertTemplateUsed(response, 'results.html')
 
-    def test_admin_team_view(self):
+    def test_no_admin_team_view(self):
         response = self.client.get(
             reverse('admin', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}))
-        self._assert_password_form(response)
+        self.assertIsPasswordForm(response)
 
-    def test_admin_per_survey_views(self):
+    def test_admin_team_view(self):
+        self.setUpAdmin()
+        response = self.client.get(
+            reverse('admin', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}))
+        self.assertContains(response, "Admin %s %s" % (self.teamtemp.id, self.team.team_name))
+        self.assertTemplateUsed(response, 'results.html')
+
+    def test_no_admin_per_survey_views(self):
         for view in ['set', 'reset', 'team']:
             response = self.client.get(reverse(view, kwargs={'survey_id': self.teamtemp.id}), follow=True)
-            self._assert_redirects(response)
+            self.assertDoesAdminRedirect(response)
 
-    def test_admin_existing_team_view(self):
+    def test_admin_per_survey_views(self):
+        self.setUpAdmin()
+        for view in ['set', 'team']:
+            response = self.client.get(reverse(view, kwargs={'survey_id': self.teamtemp.id}), follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed(response, "%s.html" % view)
+
+    def test_admin_reset_view(self):
+        self.setUpAdmin()
+        response = self.client.get(reverse('reset', kwargs={'survey_id': self.teamtemp.id}), follow=True)
+        self.assertRedirects(response, self.admin_url)
+        self.assertContains(response, "Admin %s" % self.teamtemp.id)
+        self.assertTemplateUsed(response, 'results.html')
+
+    def test_no_admin_existing_team_view(self):
         response = self.client.get(
             reverse('team', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}), follow=True)
-        self._assert_redirects(response)
+        self.assertDoesAdminRedirect(response)
+
+    def test_admin_existing_team_view(self):
+        self.setUpAdmin()
+        response = self.client.get(
+            reverse('team', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'team.html')
