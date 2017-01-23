590e482b54762b0333c1e0b673fc93ac7a71b047
Robert Loomans <robert@loomans.org>
2017-01-23 15:25:28 +1000

    reduce duplication

--- teamtemp/tests/view/test_admin_view.py
+++ teamtemp/tests/view/test_admin_view.py
@@ -11,36 +11,34 @@ class AdminViewTestCases(TestCase):
         self.team = TeamFactory(request=self.teamtemp)
         self.admin_url = reverse('admin', kwargs={'survey_id': self.teamtemp.id})
 
-    def test_admin_no_team_view(self):
-        response = self.client.get(self.admin_url)
+    def _assert_password_form(self, response)
         self.assertContains(response, 'Enter the admin password')
         self.assertTemplateUsed(response, 'password.html')
+
+    def _assert_redirects(self, response)
+        self.assertRedirects(response, self.admin_url)
+        self._assert_password_form(response)
+
+    def test_admin_no_team_view(self):
+        response = self.client.get(self.admin_url)
+        self._assert_password_form(response)
 
     def test_admin_team_view(self):
         response = self.client.get(reverse('admin', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}))
-        self.assertContains(response, 'Enter the admin password')
-        self.assertTemplateUsed(response, 'password.html')
+        self._assert_password_form(response)
 
     def test_admin_set_view(self):
         response = self.client.get(reverse('set', kwargs={'survey_id': self.teamtemp.id}), follow=True)
-        self.assertRedirects(response, self.admin_url)
-        self.assertContains(response, 'Enter the admin password')
-        self.assertTemplateUsed(response, 'password.html')
+        self._assert_redirects(response)
 
     def test_admin_reset_view(self):
         response = self.client.get(reverse('reset', kwargs={'survey_id': self.teamtemp.id}), follow=True)
-        self.assertRedirects(response, self.admin_url)
-        self.assertContains(response, 'Enter the admin password')
-        self.assertTemplateUsed(response, 'password.html')
+        self._assert_redirects(response)
 
     def test_admin_new_team_view(self):
         response = self.client.get(reverse('team', kwargs={'survey_id': self.teamtemp.id}), follow=True)
-        self.assertRedirects(response, self.admin_url)
-        self.assertContains(response, 'Enter the admin password')
-        self.assertTemplateUsed(response, 'password.html')
+        self._assert_redirects(response)
 
     def test_admin_existing_team_view(self):
         response = self.client.get(reverse('team', kwargs={'survey_id': self.teamtemp.id, 'team_name': self.team.team_name}), follow=True)
-        self.assertRedirects(response, self.admin_url)
-        self.assertContains(response, 'Enter the admin password')
-        self.assertTemplateUsed(response, 'password.html')
+        self._assert_redirects(response)
