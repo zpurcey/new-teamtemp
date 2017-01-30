981705a2e2fff2d3fe80ccb4e756bd9c37175f19
Robert Loomans <robert@loomans.org>
2017-01-30 11:53:18 +1000

    fix byte string-isms in tests

--- teamtemp/tests/view/test_robots_txt_view.py
+++ teamtemp/tests/view/test_robots_txt_view.py
@@ -1,12 +1,15 @@
+import six
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from rest_framework import status
 
 
 class RobotsTxtViewTestCases(TestCase):
     def test_robots_txt_view(self):
         response = self.client.get(reverse('robots_txt'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response['Content-Type'], 'text/plain')
         self.assertEqual(response['Cache-Control'], 'public, max-age=86400')
         self.assertTemplateNotUsed(response, 'index.html')
-        self.assertEqual(response.content, '')
+        self.assertEqual(response.content, six.b(''))
