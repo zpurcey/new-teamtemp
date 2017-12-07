c06b55c5e33c39559c9089a5c032fe4d882459a2
Robert Loomans <robert@loomans.org>
2017-12-07 12:20:26 +1000

    allow inline fonts

--- teamtemp/settings/base.py
+++ teamtemp/settings/base.py
@@ -200,7 +200,7 @@ CSP_STYLE_SRC = (
     'maxcdn.bootstrapcdn.com',
 )
 CSP_IMG_SRC = ("'self'", 'data:', 'blob:',)
-CSP_FONT_SRC = ("'self'", 'maxcdn.bootstrapcdn.com',)
+CSP_FONT_SRC = ("'self'", 'data:', 'maxcdn.bootstrapcdn.com',)
 CSP_EXCLUDE_URL_PREFIXES = ("/djadmin",)
 CSP_REPORT_URI = reverse_lazy('report_csp')
 
