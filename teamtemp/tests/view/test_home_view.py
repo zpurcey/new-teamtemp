b177ce25d25101e048de9485ab4075394420ba06
Robert Loomans <robert@loomans.org>
2018-08-11 11:16:43 +1000

    add a simple test for survey creation

--- teamtemp/tests/view/test_home_view.py
+++ teamtemp/tests/view/test_home_view.py
@@ -2,9 +2,18 @@ from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
 
+from teamtemp import utils
+from teamtemp.tests.factories import TeamTemperatureFactory
 
 class HomeViewTestCases(TestCase):
     def test_homepage_view(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'index.html')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_homepage_create_teamtemp(self):
+        password = utils.random_string(8)
+        data = {'survey_type': 'TEAMTEMP', 'new_password': password, 'confirm_password': password}
+        response = self.client.post(reverse('home'), data, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'team.html')
